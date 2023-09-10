36e64cfef917d53f780d46a2db79e83826e90385
Gentil-N <ngentil77@gmail.com>
2023-09-10 18:18:26 +0200

    update

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import cavity as cty
 from matplotlib import pyplot as plt
 import numpy as np
 
-PHOTON_CAPACITY = 2
+PHOTON_CAPACITY = 10
 
-simple_cav = cty.Cavity(1.0, 1.0, 1, PHOTON_CAPACITY, 10.0, 3.0, 1.0, True, True)
+simple_cav = cty.Cavity(1.0, 1.0, 5, PHOTON_CAPACITY, 10.0, 3.0, 1.0, True, True)
 
-psi_init = qp.tensor(qp.spin_state(1/2, 1/2), qp.fock(PHOTON_CAPACITY + 1, 0))
+psi_init = qp.tensor(qp.spin_state(5/2, 5/2), qp.fock(PHOTON_CAPACITY + 1, 0))
 
 time_range = simple_cav.run_simulation(psi_init, 0, 5, 1000)
 
-probs = simple_cav.get_all_probabilities()
+#probs = simple_cav.get_all_probabilities()
 g2 = simple_cav.compute_g2()
 
 ###############
@@ -20,13 +20,13 @@ g2 = simple_cav.compute_g2()
 
 common_title = "Simple Cavity (1 atom - 1 excitation) start |e, 0>"
 
-fig0 = plt.figure(num=0)
-ax0 = fig0.subplots(nrows=1, ncols=1)
-ax0.set_title(label=common_title)
-for prob in probs:
-    ax0.plot(time_range, prob[0], label= prob[1])
-ax0.set(xlabel="time", ylabel="probabilities")
-ax0.legend()
+#fig0 = plt.figure(num=0)
+#ax0 = fig0.subplots(nrows=1, ncols=1)
+#ax0.set_title(label=common_title)
+#for prob in probs:
+#    ax0.plot(time_range, prob[0], label= prob[1])
+#ax0.set(xlabel="time", ylabel="probabilities")
+#ax0.legend()
 
 fig4 = plt.figure(num=4)
 ax4 = fig4.subplots(nrows=1, ncols=1)
