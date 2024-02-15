4dc31074c0ae977cea3f2cf8cea3213c8eb1cd52
Gentil-N <ngentil77@gmail.com>
2024-02-15 18:24:27 +0100

    update

--- intensity-calibration.py
+++ intensity-calibration.py
@@ -12,13 +12,21 @@ ratio_ext = [0.23, 0.41, 0.46, 1.05, 1.67, 2.67, 3.67, 5.06, 6.16, 8.31, 10, 12.
 
 # Exponential approximation
 z = np.polyfit(amplitude_ext, np.log(ratio_ext), 3)
+def fit_exp(x):
+    return np.exp(z[3]) * np.exp(z[2] * x) * np.exp(z[1] * np.power(x, 2)) * np.exp(z[0] * np.power(x, 3))
 xfit = np.linspace(0, 100, 1000)
-yfit = np.exp(z[3]) * np.exp(z[2] * xfit) * np.exp(z[1] * xfit**2) * np.exp(z[0] * xfit**3) #z[2] + z[1] * xfit + z[0] * xfit**2
+yfit = fit_exp(xfit) #z[2] + z[1] * xfit + z[0] * xfit**2
 
 def theo_ratio(x, a, b):
     return a * (j1(x / 100.0 * b)/j0(x / 100.0 * b))**2
 popt, pcov = curve_fit(theo_ratio, amplitude, ratio)
 x = np.linspace(0, 100, num=1000)
+
+def alpha(x):
+    return x / (1.0 + 2.0 * x)
+
+print(yfit)
+alpha_list = alpha(yfit / 10.0) # one divides by 10 because yfit is in percent
 
 fig, ax = plt.subplots(1, 1)
 fig.set_size_inches(18.5, 10.5)
@@ -26,8 +34,27 @@ ax.plot(amplitude, ratio, "+", label="experimental data")
 ax.plot(xfit, yfit, label="exponential fit (3rd degree)")
 print(popt[0], " ", popt[1])
 ax.plot(x, theo_ratio(x, popt[0], popt[1]), label="bessel fit")
-ax.set_xlabel('Maximum  generator operation (%)')
+ax2 = ax.twinx()
+ax2.plot(xfit, alpha_list, label="alpha")
+ax2.set_ylabel('Alpha coefficient: Is = α * Itot')
+ax.set_xlabel('Amplitude modulation (%)')
 ax.set_ylabel('Sidebands ratio (%)')
 ax.legend()
+ax2.legend()
 ax.grid()
+
+fig3, ax3 = plt.subplots(1, 1)
+fig3.set_size_inches(18.5, 10.5)
+amplitude_by_ten = np.array([10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100])
+dBm_diff = [1, 4, 8, 14, 18, 28, 39, 41, 42, 45, 45, 45]
+ax3.plot(amplitude_by_ten, dBm_diff, "+", color="blue")
+ax3.set_ylabel("Power difference (43 MHz beat - noise) (dBm)", color="blue")
+ax4 = ax3.twinx()
+sideband_intensity = 10.0 * alpha(fit_exp(amplitude_by_ten) / 10)
+ax4.plot(amplitude_by_ten, sideband_intensity, "+", color="orange")
+ax4.set_ylabel("Sideband intensity (nW)", color="orange")
+ax3.set_xlabel('Amplitude modulation (%)')
+ax3.grid()
+ax3.set_title("Amplitude modulation variation at minimum total power (0.001µW from the cavity)")
+
 plt.show()
