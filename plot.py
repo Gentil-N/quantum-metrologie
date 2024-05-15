9e2332fd5326ec6c9d1fcdc09337fac9560d0dc0
Gentil-N <ngentil77@gmail.com>
2024-05-15 17:12:18 +0200

    update

--- plot.py
+++ plot.py
@@ -31,5 +31,17 @@ def plot_notch_response():
     ax1.set_xscale('log')
     plt.show()
 
+def plot_tank_response():
+    x = np.linspace(0, 100, 10000)
+    y = 1/(np.sqrt(1 + 10**2 * (1/x - x)**2))
+    fig1, ax1 = plt.subplots(1, 1)
+    fig1.set_size_inches(18.5, 10.5)
+    ax1.plot(x, y)
+    ax1.set_xlabel("ω [rad/s]")
+    ax1.set_ylabel("Gain [no unit]")
+    ax1.set_xscale('log')
+    plt.show()
+
 #plot_map_t1_greater_than_t_2()
-plot_notch_response()
+#plot_notch_response()
+plot_tank_response()
