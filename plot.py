8e8dbf2ee039829e5ff212664eca13418c1eb7f2
Gentil-N <ngentil77@gmail.com>
2024-05-16 16:56:35 +0200

    update

--- plot.py
+++ plot.py
@@ -42,6 +42,55 @@ def plot_tank_response():
     ax1.set_xscale('log')
     plt.show()
 
+def import_spec(filename):
+    file = open(filename)
+    lines = file.readlines()
+    y = []
+    x = []
+    for line in lines:
+        splitted = line.split(",")
+        x.append(float(splitted[0]))
+        y.append(float(splitted[1]))
+    file.close()
+    return [x, y]
+
+def plot_specs():
+    print("Importing..")
+    odeep = import_spec("./fastdiffamp/original_deep")
+    print("0")
+    opick = import_spec("./fastdiffamp/original_pick")
+    print("1")
+    mdeep = import_spec("./fastdiffamp/modified_deep")
+    print("2")
+    mpick = import_spec("./fastdiffamp/modified_pick")
+    print("done")
+    fig1, ax1 = plt.subplots(1, 1)
+    fig1.set_size_inches(18.5, 10.5)
+    ax1.plot(np.array(odeep[0]) / 1000000, np.array(odeep[1]))
+    ax1.set_xlabel("frequency [MHz]")
+    ax1.set_ylabel("Power [dBm]")
+
+    fig2, ax2 = plt.subplots(1, 1)
+    fig2.set_size_inches(18.5, 10.5)
+    ax2.plot(np.array(opick[0]) / 1000, opick[1])
+    ax2.set_xlabel("frequency [kHz]")
+    ax2.set_ylabel("Power [dBm]")
+
+    fig3, ax3 = plt.subplots(1, 1)
+    fig3.set_size_inches(18.5, 10.5)
+    ax3.plot(np.array(mdeep[0]) / 1000000, mdeep[1])
+    ax3.set_xlabel("frequency [MHz]")
+    ax3.set_ylabel("Power [dBm]")
+
+    fig4, ax4 = plt.subplots(1, 1)
+    fig4.set_size_inches(18.5, 10.5)
+    ax4.plot(np.array(mpick[0]) / 1000, mpick[1])
+    ax4.set_xlabel("frequency [kHz]")
+    ax4.set_ylabel("Power [dBm]")
+
+    plt.show()
+
 #plot_map_t1_greater_than_t_2()
 #plot_notch_response()
-plot_tank_response()
+#plot_tank_response()
+plot_specs()
